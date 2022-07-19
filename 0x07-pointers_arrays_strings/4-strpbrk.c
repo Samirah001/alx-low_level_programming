44f90db1c4becb3c348e4a920d82ba5bb5c60a17
samirah001 <usmansamirah001@gmail.com>
2022-07-19 13:10:57 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to search
+ * @accept: set of bytes to search for
+ * Return: pointer to byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+
+
